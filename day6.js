bec080746d9789246e6d50bb895b462c9f96a152
minakshi0808 <minakshipareek22@gmail.com>
2025-06-23 08:22:26 -0400

    Bubble Sort function

---
+++ day6.js
@@ -0,0 +1,28 @@
+//question and answers tasks
+
+// let x =[6,4,5,1,3,2] //sorting 
+// let sorted= x.sort()
+// console.log(sorted)
+//sort an given array without using sort( )
+
+// Bubble Sort function
+// function bubbleSort(arr) {
+//   // Loop over each element for multiple passes
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     // Last i elements are already in place
+//     for (let j = 0; j < arr.length - 1 - i; j++) {
+//       // Compare adjacent elements
+//       if (arr[j] > arr[j + 1]) {
+        
+//         // Swap if they are in the wrong order
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp; }}
+//   }
+//   return arr;
+// }
+
+// console.log(bubbleSort([9, 8, 7, 6, 5, 4, 3]));
+
+
+
